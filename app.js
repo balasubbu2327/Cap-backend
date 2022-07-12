02645ed445596b5b3b801fb6ba7ff5bd010a6fca
balasubbu2327 <balasubbu2327@gmail.com>
2022-07-12 21:17:38 +0530

    port

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ mongoose.connect(process.env.MONGO_URL)
 // app.use("/api",(req,res,next) => {
 //     res.send("Hello Bala")
 // })
-
-app.listen(process.env.PORT || 5000,() => {
-    console.log("Backend server is running!!!");
+const port = process.env.PORT || 5000;
+app.listen(port,() => {
+    console.log(`Listening on :${port}`);
 });
 
